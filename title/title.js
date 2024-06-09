a90e078046b5662287457a502edbdfa1b00dd11d
Bill McCutchan <bmccutchanjr@gmail.com>
2024-06-09 01:54:56 -0400

    title.html and title.js

---
+++ title/title.js
@@ -0,0 +1,9 @@
+//	TITLE.JS
+//
+//	This module collects the funvtions specific to title maintenanve (title.html).
+
+window.addEventListener ("load", event =>
+	{	event.preventDefault();
+
+alert ("title.js");
+	})
