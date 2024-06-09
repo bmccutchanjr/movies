74b46fcb02aef63ddfc097b4eeb339b898d88af7
Bill McCutchan <bmccutchanjr@gmail.com>
2024-06-09 01:44:57 -0400

    nav event handler / tweak some css

--- index.js
+++ index.js
@@ -80,12 +80,33 @@ function handleListClicks (event)
 alert ("handleListClicks ()");
 }
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //	The event handler and associated functions for click events in the nav
 
 function handleNavClicks (event)
 {	event.preventDefault();
+	let target = event.target;
 
-alert ("handleNavClicks ()");
+	//	The id for each icon is actually in the button element and there's a very good chance that that won't be the element
+	//	identified in event.target.  If not, traverse event.target's ancestors until one of them has an id attribute.
+
+	let id = undefined;
+	while (target.getAttribute ("id") == undefined)
+	{
+		target = target.parentElement;
+	}
+
+	switch (target.getAttribute ("id"))
+	{
+		case "add-a-title":
+			{
+				alert ("add-a-title");
+				break;
+			}
+		default:
+			{
+				alert ("handleNavClicks ()");
+				break;
+			}
+	}
 }
