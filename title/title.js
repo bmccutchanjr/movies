f8b8d4d4a657ba468d163e747641266463d3abd4
Bill McCutchan <bmccutchanjr@gmail.com>
2024-06-10 17:49:58 -0400

    clean up the code

--- title/title.js
+++ title/title.js
@@ -25,26 +25,25 @@ function initializeGUI ()
 	mode = "NEW TITLE";
 
 	const key = sessionStorage.getItem ("movie.key");
-//		if (key == null) initGUI (newTitle());
-if (key == null)
-{
-//	It makes no sense for the input elements to be readonly when this is a new title...traverse the DOM and remove the
-//	readonly attribute.  Then, hide the edit button.  This is exactly the same thing that has to be done when the
-//	edit button is clicked, so put that code in a function where I can get at it from the click event handler and from
-//	here
-	initGUI (newTitle());
-	editMode (true);
-}
-	else
+	if (key == null)
 	{
-		mode = "UPDATE TITLE";
-		initGUI (retrieveTitle (key));
+		//	All editable elements are readonly by default, but it makes no sense to require the user to click the
+		//	"edit" button when entering a new title.  Traverse the DOM and remove the readonly attribute.  Then, hide
+		//	the "edit" button.
 
-		//	sessionStorage is no longer needed and may cause problems later if it is allowed to persist to multiple page
-		//	loads.  So remove it now...
-
-		sessionStorage.removeItem ("movie.key");
+		initGUI (newTitle());
+		editMode (true);
 	}
+		else
+		{
+			mode = "UPDATE TITLE";
+			initGUI (retrieveTitle (key));
+
+			//	sessionStorage is no longer needed and may cause problems later if it is allowed to persist to multiple
+			//	page loads.  So remove it now...
+
+			sessionStorage.removeItem ("movie.key");
+		}
 }
 
 function initGUI (data)
@@ -80,13 +79,6 @@ function newTitle ()
 			release:	null,
 			about:		null
 		}
-//	const d =
-//		{
-//			key:		null,
-//			title:		"Lost in Translation",
-//			release:	1937,
-//			about:		"Bill <b>Murray</b> and Scarlett <b>Johansson</b> <s>parody</s> meet in Tokyo."
-//	}
 
 	return d;
 }
@@ -117,10 +109,7 @@ function handleNavClicks (event)
 			}
 		case "edit-page":
 			{
-//	Use .getElementsByTagName to retrieve a reference to all input elements.  Iterate that list and remove the attribute
-//	'readonly' from each.  Do the same for textarea elements.  There are a few div elements that need to be made editable
-//	as well.  Assign a value of true to property 'contenteditable'
-editMode (true);
+				editMode (true);
 				break;
 			}
 		case "reset-page":
@@ -143,44 +132,31 @@ function resetPage ()
 
 	resetElement (document.getElementsByTagName ("input"));
 	resetElement (document.getElementsByTagName ("textarea"));
-
-	//	There are also editable div elements (can't use textarea because I want allow some HTML tags for formatting).  They
-	//	also need to become editable, but that's slightly different.
-
-//		const div = document.querySelectorAll (".editable");
-//		for (i=0; i<div.length; i++)
-//		{
-//			div[i].setAttribute ("contenteditable", true)
-//		}
-resetElement (document.querySelectorAll (".editable"));
+	resetElement (document.querySelectorAll (".editable"));
 }
 
 function resetElement (list, _)
 {
 	//	reset the values of all editable fields to whatever value they had when the page was loaded.
 
-//		Array.from (list).forEach (l =>
 	for (i=0; i<list.length; i++)
-		{
-			//	There are editable div, input and textarea elements.  Some of the input elements may be checkboxes.  Eventually,
-			//	there will be star ratings and favorites (although I don't know how I;m going to handle those just yet.  They all
-			//	need to be handled a little differently. 
+	{
+		//	There are editable div, input and textarea elements.  Some of the input elements may be checkboxes.  Eventually,
+		//	there will be star ratings and favorites (although I don't know how I;m going to handle those just yet.  They all
+		//	need to be handled a little differently. 
 
-//				if (l.tagName == "div")
-			if (list[i].tagName == "DIV")
-			{
-//					l.innerHTML = l.getAttribute ("reset");
-				list[i].innerHTML = list[i].getAttribute ("reset");
-				continue;
-			}
+		if (list[i].tagName == "DIV")
+		{
+			list[i].innerHTML = list[i].getAttribute ("reset");
+			continue;
+		}
 //
 //	Any other "special cases" elements go here; checkbosec, radio buttons, star ratings, etc...
 //
-			//	And finally...the default behavior.  Assign the value of reset to the value attribute of the element
+		//	And finally...the default behavior.  Assign the value of reset to the value attribute of the element
 
-//				l.innerHTML = l.getAttribute ("reset");
-			list[i].value = list[i].getAttribute ("reset");
-		}
+		list[i].value = list[i].getAttribute ("reset");
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -188,12 +164,11 @@ function resetElement (list, _)
 
 function editMode (_)
 {
+	//	Remove the "readonly" attribute from editable elements (inbox, textarea, etc) so that they can, in fact, be
+	//	edited.
+
 //	This function does not take a parameter just yet -- maybe never.  The function is set up with an argument as a
 //	reminder to me, because I may want to toggle edit mode (say after saving) someday.
-
-//	Use .getElementsByTagName to retrieve a reference to all input elements.  Iterate that list and remove the attribute
-//	'readonly' from each.  Do the same for textarea elements.  There are a few div elements that need to be made editable
-//	as well.  Assign a value of true to property 'contenteditable'
 
 	toggleReadonly (document.getElementsByTagName ("input", true));
 	toggleReadonly (document.getElementsByTagName ("textarea", true));
@@ -201,8 +176,6 @@ function editMode (_)
 	//	There are also editable div elements (can't use textarea because I want allow some HTML tags for formatting).  They
 	//	also need to become editable, but that's slightly different.
 
-	// const main = document.getElementsByTagName ("main")[0];
-	// const div = main.querySelector (".editable");
 	const div = document.querySelectorAll (".editable");
 	for (i=0; i<div.length; i++)
 	{
